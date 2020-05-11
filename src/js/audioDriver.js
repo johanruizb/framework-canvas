ef1bd175ed2002b2dcb5b94a527a2ae0f3e69294
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2020-05-11 15:37:19 -0500

    implementacion de sonido incompleta

--- src/js/audioDriver.js
+++ src/js/audioDriver.js
@@ -1,16 +1,37 @@
 /**
  * @author Johan R
  */
-document.getElementById('soundtrack').click();
-document.addEventListener('click', event => {
-  console.log('Reproduciendo Soundtrack');
-  var audio = document.getElementById('soundtrack');
-  audio.play();
-});
+
+function music() {
+  document.getElementById('prueba').onclick = "";
+  const seleccion = musicRandom()
+  console.log(seleccion);
+  if (seleccion == 1) {
+    document.getElementById('blueSkyBlues').click();
+    document.addEventListener('click', event => {
+      console.log('Reproduciendo Soundtrack "Blue Sky Blues"');
+      var audio = document.getElementById('blueSkyBlues');
+      audio.play();
+    });
+  } else {
+    document.getElementById('reCreation').click();
+    document.addEventListener('click', event => {
+      console.log('Reproduciendo Soundtrack "reCreation"');
+      var audio = document.getElementById('reCreation');
+      audio.play();
+    });
+  }
+}
+
+
 
 function buttonSound() {
   var aud = document.getElementById("buttonPlay");
   console.log('Reproduciendo sonido del boton');
   aud.autoplay = true;
   aud.load();
+}
+
+function musicRandom() {
+  return Math.floor((Math.random() * 2) + 1)
 }
